b37f3749dca8aa06853b88e08314a324258a116a
lainiwa <kirrik96@gmail.com>
2019-03-05 19:22:53 +0300

    Style fixes

--- setup.py
+++ setup.py
@@ -1,9 +1,9 @@
 from setuptools import setup
 
 setup(
-    name = "webcount",
-    version = "0.1",
-    license = "BSD",
+    name='webcount',
+    version='0.1',
+    license='BSD',
     packages=['webcount', 'test'],
     install_requires=['requests'],
 )
